7f1662b71131b6b3cd8274fc6b42c216cb4e44f2
GerardoNolasco05 <nolasco.gerardo@gmail.com>
2025-02-25 15:38:12 +0100

    correct the suggestion

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,36 +1,61 @@
 import json
 
+
 def load_data(file_path):
-    """Loads JSON file"""
-    with open(file_path, 'r') as handle:
+    """Load JSON file."""
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
 
-with open("animals_template.html", "r", encoding="utf-8") as file:
-    html_template = file.read()
+def load_animal_data():
+    """Load the animals_template.html file and return its content."""
+    with open("animals_template.html", "r", encoding="utf-8") as file:
+        return file.read()
 
 
 def animal_content(animals_data):
-    output = ''
-    for animal_data in animals_data:
-        output += '<li class="cards__item">\n'
-        output += f'  <div class="card__title">{animal_data["name"]}</div>\n'
-        output += '  <p class="card__text">\n'
-        output += f'      <strong>Diet:</strong> {animal_data["characteristics"]["diet"]}<br/>\n'
-        output += f'      <strong>Location:</strong> {animal_data["locations"][0]}<br/>\n'
-        if 'type' in animal_data['characteristics']:
-            output += f'      <strong>Type:</strong> {animal_data["characteristics"]["type"]}<br/>\n'
-        output += '  </p>\n'
-        output += '</li>\n'
+    """Generate HTML list items for animals."""
+    output = []
 
-    return output
+    for animal in animals_data:
+        output.append(f'''
+        <li class="cards__item">
+            <div class="card__title">{animal["name"]}</div>
+            <p class="card__text">
+                <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>
+                <strong>Location:</strong> {animal["locations"][0]}<br/>
+                {(
+                    f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>'
+                    if "type" in animal["characteristics"]
+                    else ""
+                )}
+            </p>
+        </li>
+        ''')
+
+    return "\n".join(output)
 
 
-# Replace the placeholder with the actual animal content
-animal_info_html = animal_content(animals_data)
-html_template = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_info_html)
+def generate_animal_html(template, animals_data):
+    """Insert animal data into the HTML template."""
+    animal_info_html = animal_content(animals_data)
+    return template.replace("__REPLACE_ANIMALS_INFO__", animal_info_html)
 
-# Save the modified HTML to a new file
-with open("animals.html", "w", encoding="utf-8") as output_file:
-    output_file.write(html_template)
+
+def save_html(filename, content):
+    """Save the given content to a file."""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(content)
+
+
+def main():
+    """Main function to process animal data and generate an HTML file."""
+    animals_data = load_data("animals_data.json")
+    html_template = load_animal_data()
+    updated_html = generate_animal_html(html_template, animals_data)
+    save_html("animals.html", updated_html)
+    print("HTML file generated successfully: animals.html")
+
+
+if __name__ == "__main__":
+    main()
